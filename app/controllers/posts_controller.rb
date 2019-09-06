546a7e162f18158701e39f9ff58d72eeff0ec428
Tony Eung <eungtony@outlook.fr>
2019-09-06 18:53:22 +0200

    reformat code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,15 +27,12 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user = User.find(1)
-    respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        redirect_to "http://localhost:3000/posts/new"
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
-    end
   end
 
   # PATCH/PUT /posts/1
@@ -56,10 +53,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to "http://localhost:3000/users"
   end
 
   private
